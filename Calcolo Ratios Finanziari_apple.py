ea07fd7612fc9df082b2287b00e39d6cbf0d652d
albylorentz <albylorentz@gmail.com>
2025-03-19 12:11:51 +0100

    Aggiunto commento allo script per calcolare P/E e P/B ratios

--- Calcolo Ratios Finanziari_apple.py
+++ Calcolo Ratios Finanziari_apple.py
@@ -1,6 +1,6 @@
 apple = {
     "name":"APPLE",
-    "TK": "AAPL",
+    "tk": "AAPL",
     "prezzo": 150.50,
     "eps": 6.15,
     "book_value": 25.80
@@ -10,3 +10,5 @@ apple["pe"] = apple["prezzo"] / apple["eps"]
 apple["pb"] = apple["prezzo"] / apple["book_value"]
 
 print(apple)  # Mostra tutti i dati!
+'aggiungo un commento per verificare git'
+''
